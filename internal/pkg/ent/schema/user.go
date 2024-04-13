d643894562641a22e583e1e1763ee7fa94bc4dc2
hiyoko <62996611+hiyoko9513@users.noreply.github.com>
2024-04-13 21:47:45 +0900

    :recycle: importの最適化

--- internal/pkg/ent/schema/user.go
+++ internal/pkg/ent/schema/user.go
@@ -1,13 +1,13 @@
 package schema
 
 import (
-	"entgo.io/ent/schema/index"
 	"time"
 
 	"hiyoko-fiber/internal/pkg/ent/util"
 
 	"entgo.io/ent"
 	"entgo.io/ent/schema/field"
+	"entgo.io/ent/schema/index"
 )
 
 // User holds the schema definition for the User entity.
