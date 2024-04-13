9272a15208372ea0da8055f43007a30221331bfa
hiyoko <62996611+hiyoko9513@users.noreply.github.com>
2024-04-13 21:46:06 +0900

    :sparkles: modelsの追加

---
+++ internal/pkg/ent/models/users_model.go
@@ -0,0 +1,21 @@
+package models
+
+import (
+	"hiyoko-fiber/internal/domain/entities/users"
+	"hiyoko-fiber/internal/pkg/ent"
+)
+
+type UsersModel struct {
+	ent.User
+}
+
+func (u UsersModel) ToEntity() *users.UserEntity {
+	return &users.UserEntity{
+		ID:         u.ID.ToString(),
+		OriginalID: u.OriginalID,
+		Email:      u.Email,
+		Password:   u.Password,
+		CreatedAt:  u.CreatedAt,
+		UpdatedAt:  u.UpdatedAt,
+	}
+}
