b4c397e4a478dac26dbdbc9a6bd32ceae164725f
Fybrid <99884828+Fybrid@users.noreply.github.com>
2024-05-11 18:50:09 +0900

    :sparkles: userTableにstatusを追加 (#71)
    * :sparkles: userTableにstatusを追加
    
    * :recycle: 改行追加

--- internal/domain/entities/users/user_entity.go
+++ internal/domain/entities/users/user_entity.go
@@ -4,9 +4,26 @@ import "time"
 
 type UserEntity struct {
 	ID         string
+	Status     Status
 	OriginalID string
 	Email      string
 	Password   string
 	CreatedAt  time.Time
 	UpdatedAt  time.Time
+}
+
+type Status int8
+
+const (
+	StatusInactive   Status = 0 // inactive
+	StatusActive     Status = 1 // active
+	StatusUnverified Status = 2 // unverified
+)
+
+func (s Status) Default() Status {
+	return StatusUnverified
+}
+
+func (s Status) ToInt8() int8 {
+	return int8(s)
 }
